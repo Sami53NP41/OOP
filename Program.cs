78228e084ae769ce5156dcb9828c762e297a6fd2
Sami53NP41 <113965471+Sami53NP41@users.noreply.github.com>
2022-11-03 08:13:15 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace Kursova2
+namespace main
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        } 
+        }
     }
 }
